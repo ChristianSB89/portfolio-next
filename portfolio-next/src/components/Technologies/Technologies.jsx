20fd3b017159b5053818b5ca41a961c47b8deb0d
ChristianSB <christiansb.kodehode@gmail.com>
2022-12-07 15:53:40 +0100

    Made tech section

--- portfolio-next/src/components/Technologies/Technologies.jsx
+++ portfolio-next/src/components/Technologies/Technologies.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { DiFirebase, DiReact, DiZend } from "react-icons/di";
+import { DiHtml5, DiCss3, DiJavascript, DiReact, DiZend } from "react-icons/di";
+import { SiUnrealengine, SiTypescript } from "react-icons/si";
 import {
   Section,
   SectionDivider,
@@ -19,9 +20,29 @@ const Technologies = () => (
     <SectionDivider />
     <SectionTitle>Technologies</SectionTitle>
     <SectionText>
-      I've worked with React, NextJS, some Python and some C++ as well as the
-      basics of HTML, CSS and JS.
+      I've worked with a range of technologies in web development.
+      <br /> From design and front end to testing out some game engines.
     </SectionText>
+    <List>
+      <ListItem>
+        <DiHtml5 size="3rem" />
+      </ListItem>
+      <ListItem>
+        <DiCss3 size="3rem" />
+      </ListItem>
+      <ListItem>
+        <DiJavascript size="3rem" />
+      </ListItem>
+      <ListItem>
+        <DiReact size="3rem" />
+      </ListItem>
+      <ListItem>
+        <SiTypescript size="3rem" />
+      </ListItem>
+      <ListItem>
+        <SiUnrealengine size="3rem" />
+      </ListItem>
+    </List>
   </Section>
 );
 
